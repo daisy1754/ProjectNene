4ce8cfe31433c3171def6040e7326e6f98e78075
daisy1754 <d-part@hotmail.co.jp>
2013-05-16 01:45:43 +0900

    reload top page if fail to click element

--- simulateUser.js
+++ simulateUser.js
@@ -8,10 +8,19 @@ var dispatchMouseEvent = function(target, var_args) {
 };
 
 var simulateClick = function(element) {
-  dispatchMouseEvent(element, 'click', true, true);
+  if (element) {
+    dispatchMouseEvent(element, 'click', true, true);
+  } else {
+    // I may lost a connection, just reload top page.
+    reloadTopPage();
+  }
 }
 
 var clickLinkWithText = function(text) {
   simulateClick($("a").filter(
       function() {return $(this).text() === text;})[0]);
+}
+
+var reloadTopPage = function() {
+  location.href = "http://gree-th-www.app-amb.jp/index.php";
 }
