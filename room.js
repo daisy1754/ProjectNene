777e96f59107f6e70bab0e5ff0321638ee6d59b7
daisy1754 <d-part@hotmail.co.jp>
2013-05-16 20:29:43 +0900

    implement main stragety

--- room.js
+++ room.js
@@ -1,8 +1,11 @@
+var LEAST_ACTION_POINT = 50;
+var LEAST_DECK_COST = 65;
+
 var getCurrentInfo = function(info) {
   var actionCostInfo = $("table:first").find("td");
   if (info === 'action') {
     return parseInt($(actionCostInfo[0]).text().match(/(\d+)\//)[1]);
-  } else if (info === 'buttle') {
+  } else if (info === 'battle') {
     return parseInt($(actionCostInfo[1]).text().match(/(\d+)\//)[1]);
   }
 }
@@ -11,8 +14,18 @@ var getCurrentActionPoint = function() {
   return getCurrentInfo('action');
 }
 
-var getCurrentButtlePoint = function() {
-  return getCurrentInfo('buttle');
+var getCurrentBattlePoint = function() {
+  return getCurrentInfo('battle');
+}
+
+var hasMaxNumOfCards = function() {
+  var numOfCards = 
+      $("table:eq(3)")
+          .find("tr:eq(1)")
+          .find("td:eq(1)")
+          .text()
+          .match(/ｶｰﾄﾞ数:(\d+)\/(\d+)/);
+  return (parseInt(numOfCards[1]) / parseInt(numOfCards[2])) === 1.0;
 }
 
 var gotoPage = function(kind) {
@@ -31,3 +44,15 @@ var gotoQuestPage = function() {
   gotoPage('quest');
 }
 
+var mainStrategy = function() {
+  if (getCurrentActionPoint() > LEAST_ACTION_POINT && !hasMaxNumOfCards()) {
+    gotoQuestPage();
+  } else if (getCurrentBattlePoint() > LEAST_DECK_COST) {
+    gotoBattlePage();
+  } else {
+    // re-run after an hour
+    setTimeout(mainStrategy, 1000 * 60 * 60);
+  }
+}
+
+mainStrategy();
